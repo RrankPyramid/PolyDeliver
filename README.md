ba38f52a9578efd5055063cc8a4272d9a2051b26
R_rank_Pyramid <r_rank_pyramid@foxmail.com>
2021-11-01 19:17:20 +0800

    add gui framework

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # COMP2411-project
+
 COMP2411-project
--- src/comp2411/demo/Main.java
+++
@@ -1,7 +0,0 @@
-package comp2411.demo;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
